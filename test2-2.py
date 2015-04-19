c53c4d0279f57575cc82980191c30d0634d61ff5
18342910993 <2587045125@qq.com>
2015-04-19 22:57:23 +0800

    老师教的有点晚，麻烦老师看一下。。。

---
+++ test2-2.py
@@ -0,0 +1,23 @@
+#-*- coding:utf-8 -*-
+
+######学生基本信息############
+#姓名：韩东
+#学号：1403050209
+#班级：通风14-2班
+
+
+########题目#######
+
+物理18-5
+
+import math
+
+I0=3.0
+I1=1.0/2*I0
+I2=I1*math.cos(math.radians(30))**2
+I3=I2*math.cos(math.radians(30))**2
+I4=I3*math.cos(math.radians(30))**2
+a=I4/I1
+
+print 'I4/I1的比值为a:',a=I4/I1
+
